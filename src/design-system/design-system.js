f99961991a66e137ebedee04c108021db81e3060
Mahsa <arabzadehmah@gmail.com>
2024-05-10 04:14:42 +0430

    Add(styles): add reset-css file
    this file will reset default style of browser

--- src/design-system/design-system.js
+++ src/design-system/design-system.js
@@ -1,7 +1,7 @@
 import navBarGenerator from "../components/header/navBar";
 import selectLang from "../components/header/language";
 import headerGenerator from "../components/header/header";
+import "../styles/main.scss";
 
-headerGenerator();
-selectLang();
-navBarGenerator();
+const body = document.body;
+body.appendChild(headerGenerator());
