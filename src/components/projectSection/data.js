3bc283de4196c689272c33c8f1274bd0c32fd975
Mahsa <arabzadehmah@gmail.com>
2024-05-30 20:04:34 +0430

    Fix(projects): fix data projects to manimum show three project in landing page

--- src/components/projectSection/data.js
+++ src/components/projectSection/data.js
@@ -17,24 +17,6 @@ export const projects = [
     title: "Kahoot Answers Viewer",
     paragraph: "Get answers to your kahoot quiz",
   },
-  {
-    bannerProject: "/images/project1.png",
-    tools: "Express css Node.js",
-    title: "Kahoot Answers Viewer",
-    paragraph: "Get answers to your kahoot quiz",
-  },
-  {
-    bannerProject: "/images/project2.png",
-    tools: "Express css Node.js",
-    title: "Kahoot Answers Viewer",
-    paragraph: "Get answers to your kahoot quiz",
-  },
-  {
-    bannerProject: "/images/project3.png",
-    tools: "Express css Node.js",
-    title: "Kahoot Answers Viewer",
-    paragraph: "Get answers to your kahoot quiz",
-  },
 ];
 
 export const miniProjectsData = [
