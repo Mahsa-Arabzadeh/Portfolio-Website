b03c7c7d912cf99000ed53ae0f1d71716ad257b4
Mahsa <arabzadehmah@gmail.com>
2024-08-06 15:56:34 +0430

    Fix(images): fix images address

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
-  publicDir: "public/",
   base: "/Portfolio-Website/",
   build: {
     outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 });
